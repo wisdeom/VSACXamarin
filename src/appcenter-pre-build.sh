02f729f85d2573addd16d8396d096144385c633b
Jihye Eom <>
2018-03-23 16:55:36 +0900

    env

--- src/appcenter-pre-build.sh
+++ src/appcenter-pre-build.sh
@@ -1,4 +1,8 @@
 ﻿#!/usr/bin/env bash
-if["$APPCENTER_BRANCH"=="QA"];
-then/usr/libexec/plistbuddy -c "Set :CFBundleDisplayName VSACXamarin.$APPCENTER_BRANCH" "iOS/Info.plistbuddy"
-fi
+
+#if["$APPCENTER_BRANCH"=="QA"];
+#then/usr/libexec/plistbuddy -c "Set :CFBundleDisplayName VSACXamarin.$APPCENTER_BRANCH" "iOS/Info.plistbuddy"
+#fi
+
+
+sed -i -e "s/\[VSAC_APIKEY]/$VSAC_Key/g ../Constants.cs"
